52564f46dbc0f31b91a9d4afa0d682e523987f4a
Marcos <vinicius.paraujo3@gmail.com>
2026-09-16 21:39:24 -0300

    Gera robots.txt conforme a URL pública da imagem

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ site }) => {
+	const sitemap = new URL('/sitemap-index.xml', site);
+
+	return new Response(`User-agent: *\nAllow: /\n\nSitemap: ${sitemap}\n`, {
+		headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+	});
+};
